1ef62f9ea4e952f72b2f5cf4179600232b741e54
Luc CHAN <lchan@e1r7p3.42.fr>
2022-06-01 21:46:07 +0200

    lol

--- sl_endgame.c
+++ sl_endgame.c
@@ -6,7 +6,7 @@
 /*   By: lchan <lchan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:19:35 by lchan             #+#    #+#             */
-/*   Updated: 2022/06/01 17:43:37 by lchan            ###   ########.fr       */
+/*   Updated: 2022/06/01 21:44:28 by lchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 void	__endimage(t_data *data, t_img *img)
 {
-	mlx_destroy_image(data->mlx_ptr, img[GROUND].ptr);
-	/*mlx_destroy_image(data->mlx_ptr, img[P_UP].ptr);
+	int	i;
+
+	i = -1;
+	while (++i < SPRITES_CNT)
+		if (img[i].ptr != NULL)
+			mlx_destroy_image(data->mlx_ptr, img[i].ptr);
+	/*mlx_destroy_image(data->mlx_ptr, img[GROUND].ptr);
+	mlx_destroy_image(data->mlx_ptr, img[P_UP].ptr);
 	mlx_destroy_image(data->mlx_ptr, img[P_DOWN].ptr);
 	mlx_destroy_image(data->mlx_ptr, img[P_RIGHT].ptr);
 	mlx_destroy_image(data->mlx_ptr, img[P_LEFT].ptr);
@@ -26,6 +32,7 @@ void	__endimage(t_data *data, t_img *img)
 
 void	__endgame(t_data *data)
 {
+	//if(data->map)
 	__sl_free_tab(data->map);
 	mlx_clear_window(data->mlx_ptr, data->mlx_win);
 	__endimage(data, data->img);
